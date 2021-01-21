92745a3b6a116f7ca6349d7b03be60cfe485497a
umut azak <umutazak_0505@outlook.com>
2021-01-21 21:10:02 +0300

    MyDictionaryDemo

---
+++ MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    class MyDictionary<T>
+    {
+        T[] itmes;
+        T[] tempArray;
+       
+        public MyDictionary()
+        {
+            itmes = new T[0];
+        }
+
+        
+        public void Add(T item)
+        {
+            tempArray = itmes;
+            itmes = new T[itmes.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                itmes[1] = tempArray[i];
+            }
+
+            itmes[itmes.Length - 1] = item;
+            Console.WriteLine(item);
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> FirstName = new MyDictionary<string>();
+            FirstName.Add("Umut");
+            FirstName.Add("Kemal");
+            FirstName.Add("Mustafa");
+
+            MyDictionary<string> LastName = new MyDictionary<string>();
+            LastName.Add("Azak");
+            LastName.Add("Sancak");
+            LastName.Add("Ren");
+
+            MyDictionary<int> Tel_No = new MyDictionary<int>();
+            Tel_No.Add(123456);
+            Tel_No.Add(1234567);
+            Tel_No.Add(12345678);
+
+
+            Console.ReadKey();
+        }
+    }
+}
